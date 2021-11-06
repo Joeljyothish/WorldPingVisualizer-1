16f7d144a750a5da9e6a4eeae0dbda056ba63863
Arthri <41360489+Arthri@users.noreply.github.com>
2021-11-06 16:38:38 +0800

    docs: Complete ConfigurationManager docs

--- src/WorldPingVisualizerPlugin/Configuration/ConfigurationManager.cs
+++ src/WorldPingVisualizerPlugin/Configuration/ConfigurationManager.cs
@@ -5,14 +5,26 @@ namespace WorldPingVisualizerPlugin.Configuration
 {
     public class ConfigurationManager
     {
+        /// <summary>
+        /// Gets a value indicating whether or not the configs has loaded.
+        /// </summary>
         public bool Loaded { get; private set; }
 
+        /// <summary>
+        /// Gets an object representing visualizer settings.
+        /// </summary>
         public ConfigFile<VisualizerSettings> VisualizerConfigFile { get; private set; }
 
+        /// <summary>
+        /// Initializes a new instance of <see cref="ConfigurationManager"/>.
+        /// </summary>
         internal ConfigurationManager()
         {
         }
 
+        /// <summary>
+        /// Loads the configs.
+        /// </summary>
         public void Load()
         {
             var visualizerConfigPath = Paths.VisualizerConfigPath;
